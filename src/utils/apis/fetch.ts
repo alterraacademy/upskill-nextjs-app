41a701efc82b80657dc0df55d868c7c68e13407a
Yoga S Devanada <ydevanada@gmail.com>
2024-05-24 20:53:54 +0700

    fix: move try block inside fetch function

--- src/utils/apis/fetch.ts
+++ src/utils/apis/fetch.ts
@@ -13,27 +13,27 @@ async function request<TResponse>(
   method: "GET" | "POST" | "PUT" | "DELETE",
   requestOptions?: RequestOptions
 ) {
+  const sessionName =
+    process.env.NODE_ENV === "production"
+      ? "__Secure-authjs.sessiont-token"
+      : "authjs.session-token";
+  const options: RequestInit = {
+    ...requestOptions,
+    method,
+    headers: {
+      Cookie: `${sessionName}=${cookies().get(`${sessionName}`)?.value ?? ""}`,
+    },
+  };
+  const headers = new Headers(options.headers);
+
+  if (requestOptions?.query) {
+    url += "?" + objectToQueryString(requestOptions.query);
+  }
+  if (requestOptions?.body && !(requestOptions.body instanceof FormData)) {
+    headers.set("Content-Type", "application/json");
+  }
+
   try {
-    const sessionName =
-      process.env.NODE_ENV === "production"
-        ? "__Secure-authjs.sessiont-token"
-        : "authjs.session-token";
-    const options: RequestInit = {
-      ...requestOptions,
-      method,
-      headers: {
-        Cookie: `${sessionName}=${cookies().get(sessionName)?.value ?? ""}`,
-      },
-    };
-    const headers = new Headers(options.headers);
-
-    if (requestOptions?.query) {
-      url += "?" + objectToQueryString(requestOptions.query);
-    }
-    if (requestOptions?.body && !(requestOptions.body instanceof FormData)) {
-      headers.set("Content-Type", "application/json");
-    }
-
     const response = await fetch(BASE_URL + url, options);
 
     if (response.ok) {
